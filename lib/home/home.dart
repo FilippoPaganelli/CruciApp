015b2d144e20c304116bec0c686f87ea65bad88e
FilippoPaganelli <paganellifilippo@gmail.com>
2021-06-28 01:45:52 +0200

    minor fixes

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -92,88 +92,88 @@ class HomePage extends StatelessWidget {
                     var data = json.decode(snapshot.data.toString());
                     var nItems = data.length;
                     return ListView.separated(
-                      separatorBuilder: (context, index) {
-                        return Divider(
-                          height: 20,
-                          thickness: 1,
-                        );
-                      },
-                      padding: EdgeInsets.only(top: 10),
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          onTap: () {
-                            // ONTAP NAVIGATION:
-                            HapticFeedback.vibrate();
-                            Navigator.of(context).pushNamed(
-                              '/cw',
-                              arguments: [
-                                (index + 1).toString(),
-                                data[index]["rows"],
-                                data[index]["cols"]
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            height: 20,
+                            thickness: 1,
+                          );
+                        },
+                        padding: EdgeInsets.only(top: 10),
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            onTap: () {
+                              // ONTAP NAVIGATION:
+                              HapticFeedback.vibrate();
+                              Navigator.of(context).pushNamed(
+                                '/cw',
+                                arguments: [
+                                  (index + 1).toString(),
+                                  data[index]["rows"],
+                                  data[index]["cols"]
+                                ],
+                              );
+                            },
+                            contentPadding: EdgeInsets.only(
+                                top: 5, left: 20, right: 15, bottom: 5),
+                            title: Text(
+                              data[index]["name"],
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.bold),
+                            ),
+                            minVerticalPadding: 5,
+                            subtitle: Text(
+                              '\n' + data[index]["description"],
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            trailing: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  data[index]["language"],
+                                  style: TextStyle(
+                                      color: Colors.blueGrey[600],
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  data[index]["rows"].toString() +
+                                      ' x ' +
+                                      data[index]["cols"].toString(),
+                                  style: TextStyle(
+                                      color: Colors.blueGrey[600],
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ],
-                            );
-                          },
-                          contentPadding: EdgeInsets.only(
-                              top: 5, left: 20, right: 15, bottom: 5),
-                          title: Text(
-                            data[index]["name"],
-                            style: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
-                          ),
-                          minVerticalPadding: 5,
-                          subtitle: Text(
-                            '\n' + data[index]["description"],
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          trailing: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                data[index]["language"],
-                                style: TextStyle(
-                                    color: Colors.blueGrey[600],
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                data[index]["rows"].toString() +
-                                    ' x ' +
-                                    data[index]["cols"].toString(),
-                                style: TextStyle(
-                                    color: Colors.blueGrey[600],
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          leading: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                  width: 56,
-                                  height: 56,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Color(0xff083663).withAlpha(45),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      // NUMBER:
-                                      Text(
-                                        "#${index + 1}",
-                                        style: TextStyle(
-                                            color: Colors.blueGrey[600],
-                                            fontSize: 25),
-                                      ),
-                                    ],
-                                  )),
-                            ],
-                          ),
-                        );
-                      },
-                      itemCount: nItems,
-                    );
+                            ),
+                            leading: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                    width: 56,
+                                    height: 56,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color(0xff083663).withAlpha(45),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        // NUMBER:
+                                        Text(
+                                          "#${index + 1}",
+                                          style: TextStyle(
+                                              color: Colors.blueGrey[600],
+                                              fontSize: 25),
+                                        ),
+                                      ],
+                                    )),
+                              ],
+                            ),
+                          );
+                        },
+                        itemCount: nItems);
                   } else
                     return Center(
                       child: CircularProgressIndicator(),
